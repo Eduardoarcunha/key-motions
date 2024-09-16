e6b6fa2c010b6efe17a483f457d26116f71a6e8c
Leonardo Scarlato <leonardo.scarlato@hotmail.com>
2024-09-16 14:26:42 -0300

    feat: add test set_motions

---
+++ test_key_motions.py
@@ -0,0 +1,34 @@
+from pynput.keyboard import Key
+from key_motions import KeyMotions, HoldMotion, PressMotion
+
+def test_set_motions():
+    key_motions = KeyMotions()
+
+    motions = [
+        {
+            "name": "👍",
+            "value": Key.up,
+            "motion_type": HoldMotion(name="hold"),
+            "time_to_press": 1.0,
+        },
+        {
+            "name": "✌️",
+            "value": "z",
+            "motion_type": PressMotion(name="press", duration=0.5),
+            "time_to_press": 0.5,
+        },
+    ]
+
+    key_motions.set_motions(motions)
+
+    assert key_motions.motion_key_dict["Thumb_Up"] == {
+        "value": Key.up,
+        "motion_type": {"name": "hold"},
+        "time_to_press": 1.0,
+    }
+
+    assert key_motions.motion_key_dict["Victory"] == {
+        "value": "z",
+        "motion_type": {"name": "press", "duration": 0.5},
+        "time_to_press": 0.5,
+    }
